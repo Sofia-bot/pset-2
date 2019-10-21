ba45e959b43cbc952f415dcf2616ec403ec34de7
Sofia Regencia <SRegencia@ucvts.tec.nj.us>
2019-10-21 10:50:14 -0400

    sixth problem set

--- src/hello.js
+++ src/hello.js
@@ -1,10 +1,1 @@
-console.log("Hi! What's your name?");
-process.stdin.setEncoding('utf8');
-const name;
-process.stdin.on('readable', function() {
-    name = process.stdin.read();
-    if (name !== null) {
-        console.log("Hello ".concat(name));
-        process.exit();
-    }
-});
+console.log("Hello, world");
