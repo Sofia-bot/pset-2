6cb12892e80132939a585bbb011e6a30e1d09e38
Sofia Regencia <SRegencia@ucvts.tec.nj.us>
2019-10-21 09:40:40 -0400

    initial commit

--- src/hello.js
+++ src/hello.js
@@ -1,1 +1,10 @@
-Consolelog(Hello, planet)
+console.log("Hi! What's your name?");
+process.stdin.setEncoding('utf8');
+const name;
+process.stdin.on('readable', function() {
+    name = process.stdin.read();
+    if (name !== null) {
+        console.log("Hello ".concat(name));
+        process.exit();
+    }
+});
